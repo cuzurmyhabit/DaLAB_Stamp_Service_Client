35424d83fe8c33b216bacb5fd322909250363a72
leehyunmin <leehm2292@gmail.com>
2025-11-12 08:17:55 +0900

    add. person logo

--- src/templetes/MyPage.jsx
+++ src/templetes/MyPage.jsx
@@ -15,8 +15,11 @@ export default function MyPage() {
                         <div className="bg-[url('/assets/images/alarm.png')] w-6.5 h-7 bg-cover bg-no-repeat mr-8"></div>
                         <div className="bg-[url('/assets/images/user.png')] w-7 h-7 bg-cover bg-no-repeat mr-10"></div>
                     </div>
-
-
+                </div>
+                <div className='flex'>
+                    <a href='' className="w-9 h-12 bg-[url('/assets/images/back.png')] bg-cover mt-15 ml-35"></a>
+                    <div className="bg-[url('/assets/images/mypageperson.png')] bg-cover w-40 h-40 mt-25 ml-25"></div>
+                    
                 </div>
             </div>
         </>
