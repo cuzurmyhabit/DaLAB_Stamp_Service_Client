df10591d5bccef2112839bce56b0466bdf761513
cuzurmyhabit <s2472@e-mirim.hs.kr>
2025-11-15 14:17:27 +0900

    chore: move template files to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import LoginPage from './templetes/LoginPage'
-import LoginPage_uncorrect from './templetes/LoginPage_fail';
-import SignupPage from './templetes/SignupPage';
-import SignupPage_fail from './templetes/SignupPage_fail';
-import LoginPage_fail from './templetes/LoginPage_fail';
-import MyPage from './templetes/MyPage';
+import LoginPage from './pages/LoginPage'
+import LoginPage_uncorrect from './pages/LoginPage_fail';
+import SignupPage from './pages/SignupPage';
+import SignupPage_fail from './pages/SignupPage_fail';
+import LoginPage_fail from './pages/LoginPage_fail';
+import MyPage from './pages/MyPage';
 
 export default function App() {
   return (
