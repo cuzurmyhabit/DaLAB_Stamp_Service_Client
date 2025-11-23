09710be4949c7f221898af2c0e6d95143d94a35e
KwakJakyeong <ggyul847@gmail.com>
2025-11-24 02:12:24 +0900

    Fix: SideBar children 오타 고치기

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,8 @@
-import { Children } from "react";
 
-
-export default function SideBar({chlidren}){
+export default function SideBar({children}){
   return(
-    <div className="
-      fixed top-0 left-0
-      h-screen 
-      w-[340px]
-      border-l border-slate-200
-      bg-slate-50
-      overflow-hidden
-      bg-blue-100
-      z-1
-    ">
-      {Children}
+    <div className="fixed top-0 left-0 h-screen w-[340px] border-l border-slate-200 bg-slate-50 overflow-hidden bg-blue-100 z-1">
+      {children}
     </div>
   );
 } 
