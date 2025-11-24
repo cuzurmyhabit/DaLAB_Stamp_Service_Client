58c95f19cb3b5fe5a46cff333d5171aaa02f2418
KwakJakyeong <ggyul847@gmail.com>
2025-11-24 19:43:13 +0900

    Feat: worldMap component

--- src/components/WorldMap.jsx
+++ src/components/WorldMap.jsx
@@ -1,20 +1,56 @@
 import {useState, useContext} from "react";
-// import { useReducer } from "react";
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+
 import AuthContext from "../contexts/AuthContext";
-import { TravelAgencyContext } from "../contexts/TravelAgencyContext";
 
-export default function WorldMap(...countries){
+const geoUrl = "https://raw.githubusercontent.com/d3-geom/topojson/master/test/world-countries.json";
+
+const countryNameToCode = {
+  "South Korea": "KOR",
+  "United States of America": "USA",
+};
+
+const continentNameToCode = {
+
+}
+
+export default function WorldMap({
+  highlightedCountries = [], 
+  highlightColor = "#DAEBFF" 
+  }){
   const {user} = createContext(AuthContext); // 유저의 타입을 검사 리시버, 기버
-  const {UserTravelAgency} = createContext(UserTravelAgency);
-  const {TravelAgency} = createContext(TravelAgencyContext);
-  const [userType, setUserType] = useState(user.is_user ? "receiver" : "giver");
+  const highlightedSet = new Set(highlightedCountries);
 
-  
+  return (
+    <ComposableMap projection="geoMercator">
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map((geo) => {
+            const countryName = geo.properties.name; 
+            
+            const isHighlighted = highlightedSet.has(countryName);
 
-  return(
-    <div clasName="flex grid">
-      
-    </div>
-
+            return (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                style={{
+                  default: {
+                    fill: isHighlighted ? highlightColor : "#DAEBFF",
+                    stroke: "#06162C",
+                    outline: "none"
+                  },
+                  hover: {
+                    fill: isHighlighted ? highlightColor : "#D6D6DA",
+                    stroke: "#06162C",
+                    outline: "none"
+                  },
+                }}
+              />
+            );
+          })
+        }
+      </Geographies>
+    </ComposableMap>
   );
 }
